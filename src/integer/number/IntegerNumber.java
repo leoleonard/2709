c1860b1f3f8bc967d3576ccababfc984e567e565
Damian Juzyk <damian.juzyk@gmail.com>
2018-09-28 11:47:11 +0200

    add 6.2

--- src/integer/number/IntegerNumber.java
+++ src/integer/number/IntegerNumber.java
@@ -1,8 +1,20 @@
-//package integer.number;
-//
-//public class IntegerNumber {
-//    public static void main(String[] args) {
-//        int number;
-//
-//        }
-//    }
+package integer.number;
+import java.util.Scanner;
+
+public class IntegerNumber {
+    public static void main(String[] args) {
+        int number, result;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj liczbę całkowitą");
+        number = scan.nextInt();
+
+        result = 0;
+        while ( number != 0) {
+            result += number%10;
+            number /= 10;
+        }
+
+        System.out.println("Suma liczb z liczby całkowitej to " + result);
+        }
+    }
